865a938c8b9bec8aa96686bb7b6a24e4a429c6d3
grahaminn <grahaminnocent@gmail.com>
2023-12-17 20:41:53 +0000

    adding in linear_sarsa for task 4

--- play.py
+++ play.py
@@ -68,3 +68,21 @@ def sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
     value = q.max(axis=1)
         
     return policy, value
+
+
+def linear_sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
+    random_state = np.random.RandomState(seed)
+
+    eta = np.linspace(eta, 0, max_episodes)
+    epsilon = np.linspace(epsilon, 0, max_episodes)
+
+    theta = np.zeros(env.n_features)
+
+    for i in range(max_episodes):
+        features = env.reset()
+
+        q = features.dot(theta)
+
+        # TODO:
+
+    return theta

9ecc211f425e72c0c4c4de35da319d0134db8c91
michael-masciotra <m.masciotra@se23.qmul.ac.uk>
2023-12-30 15:36:21 +0000

    Policy evaluation, improvement and iteration now functional with comments.

--- task2.py
+++ task2.py
@@ -1,56 +1,91 @@
 import numpy as np
 
-def policy_evaluation(env, policy, gamma, theta, max_iterations):
-    value = np.zeros(env.lake.size, dtype=np.float64)
-    for i in range(max_iterations):
+def policy_evaluation(env, policy, discount_factor, theta, max_iterations):
+    # Initialize value array V to zeros
+    values = np.zeros(env.n_states)
+    for _ in range(max_iterations):
         delta = 0
-        for state in range(env.lake.size):
-            v = 0
+        for state in range(env.n_states):
+            current_value = 0
+
+            # Sum over all actions
             for action, action_prob in enumerate(policy[state]):
-                for next_state in range(env.lake.size):
-                    reward = env.r(next_state, state, action)
-                    v += action_prob * env.p(next_state, state, action) * (reward + gamma * value[next_state])
-            delta = max(delta, np.abs(v - value[state]))
-            value[state] = v
+                # Sum over all next states and rewards
+                for s_prime in range(env.n_states):
+                    # Ensure we do not execute an action in the absorbing state
+                    if state != env.absorbing_state:
+                        prob = env.p(s_prime, state, action)
+                        reward = env.r(s_prime, state, action)
+                        current_value += action_prob * prob * (reward + discount_factor * values[s_prime])
+            
+            delta = max(delta, np.abs(current_value - values[state]))
+            values[state] = current_value
+        
+        # Stopping condition
         if delta < theta:
             break
-    return value
+    return values
 
-def policy_improvement(env, value, gamma):
-    policy = np.zeros((env.lake.size, env.n_actions), dtype=np.float64)
-    for state in range(env.lake.size):
-        q_values = np.zeros(env.n_actions)
+def policy_improvement(env, value, discount_factor):
+    # Initialize policy to zeros
+    policy = np.zeros([env.n_states, env.n_actions])
+
+    # Exclude the absorbing state from the loop
+    for state in range(env.n_states - 1):
+        # Initialize an array for storing the value of each action
+        action_values = np.zeros(env.n_actions)
+
+        # Compute the value of each action
         for action in range(env.n_actions):
-            for next_state in range(env.lake.size):
-                reward = env.r(next_state, state, action)
-                q_values[action] += env.p(next_state, state, action) * (reward + gamma * value[next_state])
-        best_action = np.argmax(q_values)
+            for s_prime in range(env.n_states):
+                prob = env.p(s_prime, state, action)
+                reward = env.r(s_prime, state, action)
+                action_values[action] += prob * (reward + discount_factor * value[s_prime])
+        
+        # Select the best action
+        best_action = np.argmax(action_values)
+
+        # Set the probability of selecting the best action to 1
         policy[state, best_action] = 1.0
+
+    # No policy needed for the absorbing state, as the episode ends there
+    policy[env.absorbing_state] = 0
+
     return policy
 
 def policy_iteration(env, gamma, theta, max_iterations, policy=None):
     if policy is None:
-        policy = np.zeros((env.lake.size, env.n_actions), dtype=np.float64)
-        policy[:, 0] = 1.0  # Initialize a policy with the first action as default
+        policy = np.zeros((env.n_states, env.n_actions), dtype=np.float64)
+
+        # Initialize a policy with the first action as default
+        policy[:, 0] = 1.0
     else:
         policy = np.array(policy, dtype=np.float64)
-    for i in range(max_iterations):
+    
+    for _ in range(max_iterations):
+        # Calculate the value of the current policy using policy_evaluation
         value = policy_evaluation(env, policy, gamma, theta, max_iterations)
+
+        # Create a new policy with the calculated value using policy_improvement
         new_policy = policy_improvement(env, value, gamma)
-        if (new_policy == policy).all():
+
+        # When the old policy is equal to the new policy, we have reached the optimal policy
+        # Break out of the loop
+        if np.array_equal(new_policy, policy):
             break
+
         policy = new_policy
     return policy, value
 
 def value_iteration(env, gamma, theta, max_iterations, value=None):
-    value = np.zeros(env.lake.size, dtype=np.float64)
-    policy = np.zeros((env.lake.size, env.n_actions), dtype=np.float64)
-    for i in range(max_iterations):
+    value = np.zeros(env.n_states, dtype=np.float64)
+    policy = np.zeros((env.n_states, env.n_actions), dtype=np.float64)
+    for _ in range(max_iterations):
         delta = 0
-        for state in range(env.lake.size):
+        for state in range(env.n_states):
             q_values = np.zeros(env.n_actions)
             for action in range(env.n_actions):
-                for next_state in range(env.lake.size):
+                for next_state in range(env.n_states):
                     reward = env.r(next_state, state, action)
                     q_values[action] += env.p(next_state, state, action) * (reward + gamma * value[next_state])
             best_value = np.max(q_values)
@@ -58,10 +93,10 @@ def value_iteration(env, gamma, theta, max_iterations, value=None):
             value[state] = best_value
         if delta < theta:
             break
-    for state in range(env.lake.size):
+    for state in range(env.n_states):
         q_values = np.zeros(env.n_actions)
         for action in range(env.n_actions):
-            for next_state in range(env.lake.size):
+            for next_state in range(env.n_states):
                 reward = env.r(next_state, state, action)
                 q_values[action] += env.p(next_state, state, action) * (reward + gamma * value[next_state])
         best_action = np.argmax(q_values)
--- test-task2.py
+++ test-task2.py
@@ -19,48 +19,50 @@ class TestReinforcementLearningMethods(unittest.TestCase):
 
     def test_policy_evaluation(self):
         print("Testing Policy Evaluation...")
-        random_policy = np.random.uniform(size=(self.env.lake.size, self.env.n_actions))
+        random_policy = np.ones([self.env.n_states, self.env.n_actions])
+        random_policy[self.env.absorbing_state] = 0
         random_policy /= random_policy.sum(axis=1)[:, None]  # Normalize to get probabilities
         initial_value = np.zeros(self.env.lake.size, dtype=np.float64)
         print(f"Initial Value Function: {initial_value}")
         final_value = policy_evaluation(self.env, random_policy, self.gamma, self.theta, self.max_iterations)
         print(f"Final Value Function: {final_value}")
         self.assertTrue(isinstance(final_value, np.ndarray))
-        self.assertEqual(final_value.shape, (self.env.lake.size,))
+        self.assertEqual(final_value.shape, (self.env.n_states,))
 
     def test_policy_improvement(self):
         print("Testing Policy Improvement...")
-        value = np.zeros(self.env.lake.size)
-        initial_policy = np.zeros((self.env.lake.size, self.env.n_actions), dtype=np.float64)
+        value = np.zeros(self.env.n_states)
+        value[self.env.absorbing_state] = 0
+        initial_policy = np.zeros((self.env.n_states, self.env.n_actions), dtype=np.float64)
         print(f"Initial Policy: {initial_policy}")
         improved_policy = policy_improvement(self.env, value, self.gamma)
         print(f"Improved Policy: {improved_policy}")
         self.assertTrue(isinstance(improved_policy, np.ndarray))
-        self.assertEqual(improved_policy.shape, (self.env.lake.size, self.env.n_actions))
+        self.assertEqual(improved_policy.shape, (self.env.n_states, self.env.n_actions))
 
     def test_policy_iteration(self):
         print("Testing Policy Iteration...")
-        initial_policy = np.zeros((self.env.lake.size, self.env.n_actions), dtype=np.float64)
+        initial_policy = np.zeros((self.env.n_states, self.env.n_actions), dtype=np.float64)
         print(f"Initial Policy: {initial_policy}")
         final_policy, final_value = policy_iteration(self.env, self.gamma, self.theta, self.max_iterations)
         print(f"Final Policy: {final_policy}")
         print(f"Final Value Function: {final_value}")
         self.assertTrue(isinstance(final_policy, np.ndarray))
         self.assertTrue(isinstance(final_value, np.ndarray))
-        self.assertEqual(final_policy.shape, (self.env.lake.size, self.env.n_actions))
-        self.assertEqual(final_value.shape, (self.env.lake.size,))
+        self.assertEqual(final_policy.shape, (self.env.n_states, self.env.n_actions))
+        self.assertEqual(final_value.shape, (self.env.n_states,))
 
     def test_value_iteration(self):
         print("Testing Value Iteration...")
-        initial_value = np.zeros(self.env.lake.size)
+        initial_value = np.zeros(self.env.n_states)
         print(f"Initial Value Function: {initial_value}")
         final_policy, final_value = value_iteration(self.env, self.gamma, self.theta, self.max_iterations)
         print(f"Final Policy: {final_policy}")
         print(f"Final Value Function: {final_value}")
         self.assertTrue(isinstance(final_policy, np.ndarray))
         self.assertTrue(isinstance(final_value, np.ndarray))
-        self.assertEqual(final_policy.shape, (self.env.lake.size, self.env.n_actions))
-        self.assertEqual(final_value.shape, (self.env.lake.size,))
+        self.assertEqual(final_policy.shape, (self.env.n_states, self.env.n_actions))
+        self.assertEqual(final_value.shape, (self.env.n_states,))
 
     @staticmethod
     def main():
